4cba7f0b0c4659524a6f6e8fb0bc96a6c26a0d18
Bob Nisco <bobnisco@gmail.com>
2014-10-15 13:45:36 -0700

    First draft of using Yeoman to generate controls in addition to full app

--- app/index.js
+++ app/index.js
@@ -1,22 +1,39 @@
 var yeoman = require('yeoman-generator');
 var path = require('path');
 var chalk = require('chalk');
+var yosay = require('yosay');
 
 var AppGenerator = module.exports = yeoman.generators.Base.extend({
     constructor: function() {
         yeoman.generators.Base.apply(this, arguments);
         this.option('coffee');
+        this.generatorTypes = ['control', 'app'];
     },
 
     promptTask: function() {
         var done = this.async();
-        this.prompt({
-            type: 'input',
-            name: 'name',
-            message: 'Your OneJS view class name (e.g. FavoritesPane)',
-            default: this.appname // Default to current folder name
-        }, function(answers) {
-            this.viewName = answers.name;
+
+        // Have Yeoman greet the user.
+        this.log(yosay(
+          'Welcome to the awesome OneJS generator!'
+        ));
+
+        var prompts = [{
+                type: 'input',
+                name: 'generatorType',
+                message: 'What do you want to generate? Options are {' + this.generatorTypes.map(function(i) { return i; }) + '}',
+                default: this.generatorTypes[0] // Default to a control
+            }, {
+                type: 'input',
+                name: 'name',
+                message: 'Your OneJS control name (e.g. FavoritesPane)',
+                default: this.appname // Default to current folder name
+            }
+        ];
+
+        this.prompt(prompts, function (props) {
+            this.selectedType = props.generatorType;
+            this.viewName = props.name;
             this.viewNameMember = _toCamelCase(this.viewName);
             done();
         }.bind(this));
@@ -41,23 +58,34 @@ var AppGenerator = module.exports = yeoman.generators.Base.extend({
 
     app: function() {
         var viewPath = 'src/' + this.viewName;
-
-        this.copy('index.html');
-        this.mkdir('src');
-
-        this.copy('main.ts', 'src/main.ts');
-
-        this.mkdir('src/AppRoot');
-        this.template('src/AppRoot/AppRoot.html', 'src/AppRoot/AppRoot.html');
-        this.copy('src/AppRoot/AppRoot.less', 'src/AppRoot/AppRoot.less');
-        this.copy('src/AppRoot/AppRootBase.ts', 'src/AppRoot/AppRootBase.ts');
-        this.copy('src/AppRoot/AppRootModel.ts', 'src/AppRoot/AppRootModel.ts');
-
         this.mkdir(viewPath);
-        this.template('src/View/View.html', viewPath + '/' + this.viewName + '.html');
-        this.template('src/View/View.less', viewPath + '/' + this.viewName + '.less');
-        this.template('src/View/ViewBase.ts', viewPath + '/' + this.viewName + 'Base.ts');
-        this.template('src/View/ViewModel.ts', viewPath + '/' + this.viewName + 'Model.ts');
+
+        if (this.selectedType === this.generatorTypes[0]) {
+            // Template and copy over the source files
+            this.template('src/Control/src/_Control.html', viewPath + '/' + this.viewName + '.html');
+            this.template('src/Control/src/_Control.less', viewPath + '/' + this.viewName + '.less');
+            this.template('src/Control/src/_ControlBase.ts', viewPath + '/' + this.viewName + 'Base.ts');
+            this.template('src/Control/src/_ControlModel.ts', viewPath + '/' + this.viewName + 'Model.ts');
+
+            // Template and copy over the test stub file
+            this.template('src/Control/test/_Control.test.ts', 'test/' + this.viewName + '.test.ts');
+        } else if (this.selectedType === this.generatorTypes[1]) {
+            this.copy('index.html');
+            this.mkdir('src');
+
+            this.copy('main.ts', 'src/main.ts');
+
+            this.mkdir('src/AppRoot');
+            this.template('src/AppRoot/AppRoot.html', 'src/AppRoot/AppRoot.html');
+            this.copy('src/AppRoot/AppRoot.less', 'src/AppRoot/AppRoot.less');
+            this.copy('src/AppRoot/AppRootBase.ts', 'src/AppRoot/AppRootBase.ts');
+            this.copy('src/AppRoot/AppRootModel.ts', 'src/AppRoot/AppRootModel.ts');
+
+            this.template('src/View/View.html', viewPath + '/' + this.viewName + '.html');
+            this.template('src/View/View.less', viewPath + '/' + this.viewName + '.less');
+            this.template('src/View/ViewBase.ts', viewPath + '/' + this.viewName + 'Base.ts');
+            this.template('src/View/ViewModel.ts', viewPath + '/' + this.viewName + 'Model.ts');
+        }
     },
 
     install: function() {
---
+++ app/templates/src/Control/test/_Control.test.ts
@@ -0,0 +1,11 @@
+/// <reference path="../definitions/definitions.d.ts" />
+
+import chai = require("chai");
+import List = require("../src/List");
+import <%= viewName %> = require("../src/<%= viewName %>");
+
+var expect = chai.expect;
+
+describe('<%= viewName %>', function() {
+
+});
