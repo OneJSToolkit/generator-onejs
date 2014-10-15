72f13b9725a25ce7d29dab08edc752b4cbecad91
Bob Nisco <bobnisco@gmail.com>
2014-10-15 14:18:22 -0700

    Fix npm failure on creation of app

--- app/index.js
+++ app/index.js
@@ -39,23 +39,6 @@ var AppGenerator = module.exports = yeoman.generators.Base.extend({
         }.bind(this));
     },
 
-    gulpfile: function() {
-        this.template('gulpfile.js');
-    },
-
-    git: function() {
-        this.copy('gitignore', '.gitignore');
-        this.copy('gitattributes', '.gitattributes');
-    },
-
-    package: function() {
-        this.template('_package.json', 'package.json');
-    },
-
-    editorConfig: function() {
-        this.copy('editorconfig', '.editorconfig');
-    },
-
     app: function() {
         var viewPath = 'src/' + this.viewName;
         this.mkdir(viewPath);
@@ -85,23 +68,13 @@ var AppGenerator = module.exports = yeoman.generators.Base.extend({
             this.template('src/View/View.less', viewPath + '/' + this.viewName + '.less');
             this.template('src/View/ViewBase.ts', viewPath + '/' + this.viewName + 'Base.ts');
             this.template('src/View/ViewModel.ts', viewPath + '/' + this.viewName + 'Model.ts');
+
+            gulpfile.apply(this);
+            git.apply(this);
+            editorConfig.apply(this);
+            package.apply(this);
+            install.apply(this);
         }
-    },
-
-    install: function() {
-        var howToInstall =
-            '\nAfter running `npm install install`, inject your front end dependencies into' +
-            '\nyour HTML by running:' +
-            '\n' +
-            chalk.yellow.bold('\n  gulp wiredep');
-
-        if (this.options['skip-install']) {
-            this.log(howToInstall);
-            return;
-        }
-        var done = this.async();
-
-        this.npmInstall(null, null, done);
     }
 });
 
@@ -112,3 +85,35 @@ function _toCamelCase(val) {
 
     return val;
 }
+
+var gulpfile = function() {
+    this.template('gulpfile.js');
+};
+
+var git = function() {
+    this.copy('gitignore', '.gitignore');
+    this.copy('gitattributes', '.gitattributes');
+};
+
+var package = function() {
+    this.template('_package.json', 'package.json');
+};
+
+var editorConfig = function() {
+    this.copy('editorconfig', '.editorconfig');
+};
+
+var install = function() {
+    var howToInstall =
+        '\nAfter running `npm install install`, inject your front end dependencies into' +
+        '\nyour HTML by running:' +
+        '\n' +
+        chalk.yellow.bold('\n  gulp wiredep');
+
+    if (this.options['skip-install']) {
+        this.log(howToInstall);
+        return;
+    }
+
+    this.npmInstall();
+};
