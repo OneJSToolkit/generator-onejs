9fb3c720c40f5dd8d37b717954f4afcf5d48ebbc
Bob Nisco <BobNisco@gmail.com>
2014-10-26 21:56:26 -0700

    Lots of changes to standardize Gulp file

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,9 +3,8 @@
 var gulp = require('gulp');
 var onejsCompiler = require('gulp-onejs-compiler');
 var tsc = require('gulp-typescript');
-var clean = require('gulp-rimraf');
+var del = require('del');
 var uglify = require('gulp-uglifyjs');
-var add = require('gulp-add-src');
 var less = require('gulp-less');
 var cssMinify = require('gulp-minify-css');
 var csstojs = require('gulp-csstojs');
@@ -13,39 +12,43 @@ var filter = require('gulp-filter');
 var size = require('gulp-size');
 var mergeStream = require('merge-stream');
 var karma = require('karma').server;
+var shell = require('gulp-shell');
 
+/** The paths we'll be using for our build system */
 var paths = {
     tempPath: 'temp',
     appPath: 'app',
+    appAmdPath: 'app/amd',
+    appCommonJsPath: 'app/commonjs',
     appMinPath: 'app-min',
-    staticFiles: ['node_modules/requirejs/require.js']
+    staticFiles: ['node_modules/requirejs/require.js'],
+    testBinPath: 'bin'
 };
 
-var amdDependencies = [
-    'onejs'
+/** What standard of ECMAScript we will target for TypeScript compilation */
+var esTarget = 'ES5';
+
+/** What dependencies we'll need to manually handle */
+var dependencies = [
+    'onejs',
 ];
 
-gulp.task('clean', function() {
-    return gulp.src([paths.tempPath, paths.appPath, paths.appMinPath])
-        .pipe(clean());
+/** Cleans both app and test output files */
+gulp.task('clean', ['clean-app', 'clean-test'], function() {
+
 });
 
-gulp.task('copy-deps', ['clean'], function() {
-    var stream = mergeStream();
-
-    for (var i = 0; i < amdDependencies.length; i++) {
-        var dep = amdDependencies[i];
-
-        stream.add(
-            gulp.src('node_modules/' + dep + '/dist/amd/*')
-            .pipe(gulp.dest(paths.tempPath + '/ts/' + dep))
-            .pipe(gulp.dest(paths.appPath + '/' + dep))
-        );
-    }
-
-    return stream;
+/** Cleans app source related files */
+gulp.task('clean-app', function(cb) {
+    del([paths.tempPath, paths.appPath, paths.appMinPath], cb);
 });
 
+/** Cleans test related files */
+gulp.task('clean-test', function(cb) {
+    del([paths.testBinPath], cb);
+});
+
+/** Runs LESS compiler, CSS to JS, OneJS Compiler and outputs to temp folder */
 gulp.task('tsc-preprocess', ['copy-deps'], function() {
     var lessFilter = filter('**/*.less');
 
@@ -61,16 +64,32 @@ gulp.task('tsc-preprocess', ['copy-deps'], function() {
         .pipe(gulp.dest(paths.tempPath + '/ts'));
 });
 
+/** Takes the TS from the temp folder and compiles it to AMD JS */
 gulp.task('tsc', ['tsc-preprocess'], function() {
     return gulp.src(paths.tempPath + '/ts/**/*.ts')
         .pipe(tsc({
-            module: 'amd'
+            module: 'amd',
+            target: esTarget
         }))
-        .pipe(gulp.dest(paths.appPath));
+        .pipe(gulp.dest(paths.appAmdPath));
 });
 
+/** Takes the TS from the temp folder and compiles it to CommonJS */
+gulp.task('tsc-commonjs', ['tsc-preprocess'], function() {
+    return gulp.src([
+            paths.tempPath + '/ts/**/*.ts',
+            '!' + paths.tempPath + '/ts/{test,test/**}' // Do not match test files
+        ])
+        .pipe(tsc({
+            module: 'commonjs',
+            target: esTarget
+        }))
+        .pipe(gulp.dest(paths.appCommonJsPath));
+});
+
+/** Minifies and gzips your application */
 gulp.task('minify', ['tsc'], function() {
-    return gulp.src([paths.appPath + '/*.js'])
+    return gulp.src([paths.appPath + '/**/*.js'])
         .pipe(uglify())
         .pipe(size({
             gzip: true
@@ -78,21 +97,65 @@ gulp.task('minify', ['tsc'], function() {
         .pipe(gulp.dest(paths.appMinPath));
 });
 
+/** Copies dependencies */
+gulp.task('copy-deps', ['copy-dts'], function() {
+    var stream = mergeStream();
+
+    for (var i = 0; i < dependencies.length; i++) {
+        var dep = dependencies[i];
+
+        stream.add(
+            gulp.src('node_modules/' + dep + '/dist/amd/*')
+            .pipe(gulp.dest(paths.tempPath + '/ts/' + dep))
+            .pipe(gulp.dest(paths.appPath + '/' + dep))
+        );
+    }
+
+    return stream;
+});
+
+/** Fetches the latest typings from the DefinitelyTyped repo */
+gulp.task('fetch-dts', function() {
+    var tsdPath = './node_modules/tsd/build/cli.js ';
+    var dtsToFetch = ['chai', 'mocha']
+    var shellCommands = dtsToFetch.map(function(i) {
+        return tsdPath + 'query --action install ' + i + ' --save';
+    });
+
+    return gulp.src('package.json', {read: false})
+        .pipe(shell(shellCommands));
+});
+
+/** Copies the distributable js files of your app into the bin folder */
 gulp.task('copy-dist', ['tsc-commonjs'], function() {
-    return gulp.src('dist/commonjs/*.js')
+    return gulp.src(paths.appCommonJsPath + '/*.js')
         .pipe(gulp.dest('bin/src'));
 });
 
-gulp.task('tsc-test', ['clean-test', 'copy-dist'], function() {
-    var tsResult = gulp.src('test/*.ts')
+/** Copies the .d.ts files to the temp folder */
+gulp.task('copy-dts', function() {
+    return gulp.src('typings/**/*.d.ts')
+        .pipe(gulp.dest(paths.tempPath + '/ts/typings'));
+});
+
+/** Builds source and test files */
+gulp.task('tsc-test', ['tsc-commonjs', 'test-preprocess'], function() {
+    var tsResult = gulp.src(paths.tempPath + '/ts/test/*.ts')
         .pipe(tsc({
             module: 'commonjs',
-            target: 'ES5'
+            target: esTarget
         }));
 
     return tsResult.js.pipe(gulp.dest('bin/test'));
 });
 
+/** Copies the test files to the temp path */
+gulp.task('test-preprocess', function() {
+    return gulp.src(['test/*.ts'])
+        .pipe(gulp.dest(paths.tempPath + '/ts/test'));
+});
+
+/** Runs the test suite on your test cases */
 gulp.task('test', ['tsc-test'], function (done) {
     karma.start({
         configFile: __dirname + '/karma.conf.js',
@@ -100,7 +163,8 @@ gulp.task('test', ['tsc-test'], function (done) {
     }, done);
 });
 
-gulp.task('copy-static-files', ['clean', 'tsc'], function() {
+/** Copies the static files required for your app */
+gulp.task('copy-static-files', ['tsc'], function() {
     return gulp.src(paths.staticFiles)
         .pipe(gulp.dest(paths.appPath))
         .pipe(uglify())
@@ -111,6 +175,7 @@ gulp.task('serve', ['default'], function() {
 
 });
 
+/** Watches your source folder for changes and runs the default task */
 gulp.task('watch', function() {
     gulp.watch('src/**/*', ['default']);
 });
